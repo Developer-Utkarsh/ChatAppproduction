08db5d27ebbbd4ce09434cf591a7bb54c54cee23
Utkarsh Web <128823414+Utkarsh-Web-2023@users.noreply.github.com>
2023-08-14 19:48:25 +0530

    Final Release

--- index.js
+++ index.js
@@ -1,44 +1,50 @@
 const express = require('express');
-const static = require('static')
 const app = express();
-const http = require('http').createServer(app)
-const PORT = process.env.PORT || 8000
+
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const path = require('path');
+const router = express.Router();
+
+const PORT = process.env.PORT || 8000;
+
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.use(express.static(__dirname + '/public'))
-
-
-
-
-
-
-const io = require('socket.io')(http);
-
 const systemGreeting = {
-    user: 'Jarvis',
-    message: `Welcome to Utkarsh Chat APP `
+    user: 'ZARVIS',
+    message: 'Welcome to Utkarsh Chat APP'
 };
 
-io.on('connection', (socket) => {
-    socket.broadcast.emit('message', systemGreeting);
-    console.log("connected..");
+let userCount = 0;
 
-    socket.on("message", (msg) => {
+io.on('connection', (socket) => {
+    socket.emit('message', systemGreeting);
+    userCount++;
+    io.emit('userCount', userCount);
+
+    console.log('Connected...');
+
+    socket.on('message', (msg) => {
         socket.broadcast.emit('message', msg);
     });
 
     socket.on('joined', (name) => {
         const systemMessage = {
-            user: 'Jarvis',
+            user: 'ZARVIS',
             message: `${name} has joined the chat.`
         };
         
-        socket.broadcast.emit('message', systemMessage);
+        io.emit('message', systemMessage);
+    });
+
+    socket.on('disconnect', () => {
+        userCount--;
+        io.emit('userCount', userCount);
     });
 });
 
 http.listen(PORT, () => {
-    console.log(`Listening on localhost:${PORT}`)
-})
+    console.log(`Example app listening on port ${PORT}`);
+});
