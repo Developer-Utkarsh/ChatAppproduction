a22c1894b851acbec3f2a8ec477dca00b09a45fe
Utkarsh Web <128823414+Utkarsh-Web-2023@users.noreply.github.com>
2023-09-08 20:18:13 +0530

    New Features and Improved UI

--- index.js
+++ index.js
@@ -2,111 +2,101 @@ const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
 const path = require('path');
-let name;
+
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
+app.get('/socket.io/socket.io.js', (req, res) => {
+  res.sendFile(path.join(__dirname, 'node_modules/socket.io/client-dist/socket.io.js'));
+});
+
 const PORT = process.env.PORT || 8000;
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 const systemGreeting = {
-    user: 'ZARVIS',
-    message: 'Welcome to Utkarsh Chat APP'
+  user: 'ZARVIS',
+  message: 'Welcome to Utkarsh Chat APP'
 };
 
-
 let userCount = 0;
-let activeUsers = []; // Declare the activeUsers array here
-// const openai = require("openai");
+let activeUsers = {};
 
-// const apiKey = "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";
-// const configuration = new Configuration({
-//   apiKey: apiKey,
-// });
-
-
-async function generateBotResponse(userMessage) {
+function generateBotResponse(userMessage) {
   try {
-    // const openai = new OpenAIApi(configuration);
-    const prompt = `User: ${userMessage}\nBot:`;
-    
-    
+    // Implement your bot response generation logic here
+    // You've left this part empty, so I can't correct it
   } catch (error) {
-    // console.error("Error generating bot response:", error);
-    // return "An error occurred while generating a response.";
+    console.error("Error generating bot response:", error);
+    return "An error occurred while generating a response.";
   }
 }
 
-// console.log(OpenAIApi);
-
-
-const testMessage = "ZARVIS, tell me about yourself.";
-const botResponse = generateBotResponse(testMessage);
-// console.log("Bot Response:", botResponse);
-
 io.on('connection', (socket) => {
-    socket.emit('message', systemGreeting);
-    userCount++;
+  socket.emit('message', systemGreeting);
+  userCount++;
+  io.emit('userCount', userCount);
+
+  console.log('Connected...');
+
+  socket.on('message', (msg, targetUser) => {
+    const userSocket = activeUsers[targetUser];
+    if (userSocket) {
+      io.to(userSocket).emit('message', msg);
+    }
+    if (msg.user === 'ZARVIS' && msg.message.includes('ZARVIS')) {
+      const botResponse = generateBotResponse(msg.message);
+      const botMessage = {
+        user: 'ZARVIS',
+        message: botResponse
+      };
+      io.emit('message', botMessage);
+    } else {
+      socket.broadcast.emit('message', msg);
+    }
+  });
+
+  socket.on('image', (msg) => {
+    io.emit('image', msg);
+  });
+
+  socket.on('typing', (data) => {
+    socket.broadcast.emit('typing', data);
+  });
+
+  socket.on('joined', (username) => {
+    activeUsers[username] = socket.id;
+    const systemMessage = {
+      user: 'ZARVIS',
+      message: `On this chat, ${username} has joined.`
+    };
+    io.emit('message', systemMessage);
+    io.emit("userListUpdate", Object.keys(activeUsers));
+  });
+
+  socket.on('dmMessage', (msg, receiver) => {
+    const sender = msg.user;
+    const receiverSocketId = activeUsers[receiver];
+    if (receiverSocketId) {
+      io.to(receiverSocketId).emit('dmMessage', msg, sender);
+    }
+  });
+
+  socket.on('disconnect', () => {
+    userCount--;
+    const userToRemove = Object.keys(activeUsers).find(user => activeUsers[user] === socket.id);
+    if (userToRemove) {
+      delete activeUsers[userToRemove];
+      io.emit('userListUpdate', Object.keys(activeUsers));
+      console.log(`${userToRemove} disconnected`);
+    }
     io.emit('userCount', userCount);
-
-    console.log('Connected...');
-
-    socket.on('message', (msg) => {
-        if (msg.user === 'ZARVIS') {
-            // Handle bot activation
-            console.log("arrived to api")
-
-            if (msg.message.includes('ZARVIS')) {
-                const botResponse = generateBotResponse(msg.message);
-                const botMessage = {
-                    user: 'ZARVIS',
-                    message: botResponse
-
-                };
-                console.log("arrived to ai")
-                io.emit('message', botMessage);
-            }else{
-
-            }
-            
-        } else {
-            socket.broadcast.emit('message', msg);
-        }
-    });
-
-    socket.on('joined', (userName) => {
-
-        name = userName;
-        activeUsers.push(name); // Add the user to activeUsers array
-        const systemMessage = {
-            user: 'ZARVIS',
-            message: `On this chat ${name} has joined.`
-        };
-
-        io.emit('message', systemMessage);
-        io.emit("userListUpdate", activeUsers); // Update active user list
-    });
-
-    socket.on('disconnect', () => {
-        userCount--;
-        io.emit('userCount', userCount);
-
-        if (name) {
-            activeUsers = activeUsers.filter((user) => user !== name); // Remove user from activeUsers
-            const systemMessage = {
-                user: 'ZARVIS',
-                message: `${name} has left the chat.`
-            };
-            io.emit('message', systemMessage);
-            io.emit("userListUpdate", activeUsers); // Update active user list
-        }
-    });
+  });
 });
 
 server.listen(PORT, () => {
-    console.log(`Example app listening on port ${PORT}`);
+  console.log(`Example app listening on port ${PORT}`);
 });
